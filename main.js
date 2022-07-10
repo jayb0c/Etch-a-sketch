b8c6c2891b2405f4a2b5a023f67bfef774a21f49
Jake Stone <Stonedev12@gmail.com>
2022-07-09 18:48:31 -0700

    I have built the Etch-a-sketch, now I am starting to handle user input.

--- main.js
+++ main.js
@@ -1,20 +1,46 @@
+data = {
+  sixteen: true,
+  thirtyTwo: false,
+  sixtyFour: false
+}
+
+//block.style.setProperty('width', 'calc(100% / 16)');
+//block.style.setProperty('height', 'calc(100% / 16)');
+
 const screen = document.getElementById('screen');
 let size = '5px';
+const sixteen = 256;
+const thirtyTwo = 1024;
+const sixtyFour = 4096;
 
 document.addEventListener('DOMContentLoaded', load());
 
 function load(){
-  let row = document.createElement('row');
-  row.className = 'row';
-
-  for(let i = 0; i < 16; i++){
-    let block = document.createElement('div');
-    block.className = 'block';
-    row.appendChild(block);
+  if(data.sixteen === true){
+    for (let i = 0; i < sixteen; i++) {
+      let block = document.createElement('div');
+      block.className = 'block';
+      block.style.setProperty('width', 'calc(100% / 16)');
+      block.style.setProperty('height', 'calc(100% / 16)');
+      screen.appendChild(block);
+    }
   }
-
-  for (let i = 0; i < 16; i++) {
-    screen.appendChild(row);
+  if(data.thirtyTwo === true){
+    for (let i = 0; i < thirtyTwo; i++) {
+      let block = document.createElement('div');
+      block.className = 'block';
+      block.style.setProperty('width', 'calc(100% / 32)');
+      block.style.setProperty('height', 'calc(100% / 32)');
+      screen.appendChild(block);
+    }
   }
-
+  if(data.sixtyFour === true){
+    for (let i = 0; i < sixtyFour; i++) {
+      let block = document.createElement('div');
+      block.className = 'block';
+      block.style.setProperty('width', 'calc(100% / 16)');
+      block.style.setProperty('height', 'calc(100% / 64)');
+      screen.appendChild(block);
+    }
+  }
 }
