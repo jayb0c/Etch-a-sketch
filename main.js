7252ed16af500220a7205dbff83cc7980498c3da
Jake Stone <Stonedev12@gmail.com>
2022-07-09 20:12:34 -0700

    I have set four separate grid sizes. Next step will be setting 4 zones on the selector bar that are assigned to the grid sizes.

--- main.js
+++ main.js
@@ -1,19 +1,51 @@
 data = {
-  sixteen: true,
+  sixteen: false,
+  twenty: true,
   thirtyTwo: false,
   sixtyFour: false
 }
 
-//block.style.setProperty('width', 'calc(100% / 16)');
-//block.style.setProperty('height', 'calc(100% / 16)');
-
 const screen = document.getElementById('screen');
+const selector = document.getElementById('marker');
 let size = '5px';
 const sixteen = 256;
+const twenty = 400;
 const thirtyTwo = 1024;
 const sixtyFour = 4096;
 
 document.addEventListener('DOMContentLoaded', load());
+
+
+let newPosX = 0, newPosY = 0, startPosX = 0, startPosY = 0;
+// when the user clicks down on the element
+selector.addEventListener('mousedown', function (e) {
+  e.preventDefault();
+
+  // get the starting position of the cursor
+  startPosX = e.clientX;
+
+  document.addEventListener('mousemove', mouseMove);
+
+  document.addEventListener('mouseup', function () {
+    document.removeEventListener('mousemove', mouseMove);
+  });
+
+});
+
+
+function mouseMove(e) {
+  // calculate the new position
+  newPosX = startPosX - e.clientX;
+
+  // with each move we also want to update the start X and Y
+  startPosX = e.clientX;
+
+  // set the element's new position:
+  console.log(selector.offsetLeft - newPosX);
+  if (selector.offsetLeft - newPosX > 15 && selector.offsetLeft - newPosX < 140 ){
+  selector.style.left = (selector.offsetLeft - newPosX) + "px";
+  }
+}
 
 function load(){
   if(data.sixteen === true){
@@ -22,6 +54,15 @@ function load(){
       block.className = 'block';
       block.style.setProperty('width', 'calc(100% / 16)');
       block.style.setProperty('height', 'calc(100% / 16)');
+      screen.appendChild(block);
+    }
+  }
+  if (data.twenty === true) {
+    for (let i = 0; i < twenty; i++) {
+      let block = document.createElement('div');
+      block.className = 'block';
+      block.style.setProperty('width', 'calc(100% / 20)');
+      block.style.setProperty('height', 'calc(100% / 20)');
       screen.appendChild(block);
     }
   }
@@ -38,7 +79,7 @@ function load(){
     for (let i = 0; i < sixtyFour; i++) {
       let block = document.createElement('div');
       block.className = 'block';
-      block.style.setProperty('width', 'calc(100% / 16)');
+      block.style.setProperty('width', 'calc(100% / 64)');
       block.style.setProperty('height', 'calc(100% / 64)');
       screen.appendChild(block);
     }
